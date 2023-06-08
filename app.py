9b3717eb63ab40ec92277f363dc95e341db5bfe6
Jakub Styn <s22449@pjwstk.edu.pl>
2023-06-08 20:37:31 +0200

    Utworzenie strony, bazy danych i wstępne ostylowanie

--- app.py
+++ app.py
@@ -1,12 +1,29 @@
-from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+from flask import Flask, render_template
 
 app = Flask(__name__)
+app.secret_key = "firmex"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///firmex.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+
+class Company(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    number_of_opinions = db.Column(db.Integer, nullable=False)
+    opinions = db.Column(db.Float, nullable=False)
+    image_path = db.Column(db.String, nullable=False)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    companies = Company.query.all()
+
+    return render_template('index.html', companies=companies)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
